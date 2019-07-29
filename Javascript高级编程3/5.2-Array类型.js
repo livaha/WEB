446a70a2fc6fc4791d80e73ff2c1691618bf674b
livaha； <1192137406@qq.com>
2019-07-29 19:15:43 +0800

    Array类型

---
+++ Javascript高级编程3/5.2-Array类型.js
@@ -0,0 +1,106 @@
+/**
+ * Array类型
+ * 0 创建数组
+ * 1 检测数组:instanceof,isArray()
+ * 2 转换方法:toLocaleString(),toString(),valueOf(),join()
+ * 3 栈方法：push(),pop()
+ */
+{/**0 创建数组有2种方式
+    * 一是Array，二是方括号 */
+
+    let col1 = new Array();
+    let col2 = new Array(20);                   //创建一个包含20项的数组
+    let col3 = new Array('red','green','blue'); //创建一个包含3个字符串值的数组
+    //省略new结果一相
+    let col4 = Array(3);
+    let col5 = Array('red');
+
+    let col6 = ['red','green','blue'];
+    let col7 = [];      //创建一个空数组
+    let col8 = [1,2,];  //不要这样，这样会创建2或3项的数组
+    let col9 = [,,,,,]  //不要这样，这样会创建5或6项的数组
+
+    let colors = ['red','green','blue'];
+    colors[1] = 'hello'; //修改了索引为1的元素
+    console.log(
+        colors[0],//读取索引为0的元素 //打印red
+        colors.length, //3
+        colors[1],//hello
+    )
+
+    //通过.length修改数组长度
+    colors.length = 2;
+    console.log(colors[2]);//undefined
+
+    let arr = ['a','b','c'];
+    arr.length = 4;
+    console.log(arr[3]);//undefined
+    
+    //通过length属性在数组末尾增添值
+    arr[arr.length]='x';
+    arr[arr.length]= 'y';
+    console.log(arr);//[ 'a', 'b', 'c', <1 empty item>, 'x', 'y' ]
+
+    //通过索引赋值创建新长度
+    let testarr = ['a','b','c'];
+    testarr[6]='k';
+    console.log(testarr)//[ 'a', 'b', 'c', <3 empty items>, 'k' ]
+}
+{/**1 检测数组:instanceof,isArray() */
+    let value = ['c']
+    /**instanceof 用于一个网页或者一个全局作用域来说是不是一个数组*/
+    if(value instanceof Array){
+
+    }
+    /**isArray() 解决网页中包含多个框架，不管它在哪个全局执行环境中创建，都可以确实它是不是数组 */
+    if(Array.isArray(value)){
+        //IE9+支持
+    }
+}
+{/**2 转换方法 */
+    /**所有对象都具有toLocaleString(),toString(),valueOf(),join() */
+    let colors = ['red','green','blue'];
+    console.log(
+        colors.toString(),      //'red,green,blue'
+        colors.toLocaleString(),//'red,green,blue'
+        colors.valueOf(),       //[ 'red', 'green', 'blue' ] //数组的每一
+        colors,                 //[ 'red', 'green', 'blue' ]
+        colors.join(','),       //'red,green,blue'
+        colors.join('||'),      //'red||green||blue'
+    )
+
+    let person1 = {
+        toLocaleString:function(){
+            return 'Nicka';
+        },
+        toString:function(){
+            return 'Nickb';
+        }
+    }
+    let person2 = {
+        toLocaleString:function(){
+            return 'PooleA';
+        },
+        toString:function(){
+            return 'PooleB'
+        }
+    }
+    let people = [person1,person2];
+    console.log(
+        people,
+        people.toString(),//'Nickb,PooleB'
+        people.toLocaleString(),//'Nicka,PooleA'
+    )
+}
+{/**3 栈方法：push(),pop() */
+    let colors = new Array();
+    let count = colors.push('red','green');
+    console.log(count);//2
+
+    count = colors.push('black');
+    console.log(count);//3
+
+    let item = colors.pop();
+    console.log(item);//'black'
+    console.log(colors.length)//2
+}
