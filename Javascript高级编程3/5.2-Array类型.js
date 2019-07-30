83357010337b89eb15b19486164b75b14dae0a27
livaha； <1192137406@qq.com>
2019-07-30 10:44:47 +0800

    Update 5.2-Array类型.js

--- Javascript高级编程3/5.2-Array类型.js
+++ Javascript高级编程3/5.2-Array类型.js
@@ -158,3 +158,98 @@
     console.log(nums)//[ 1, 2, 3, 5, 6, 9, 12, 14, 21 ]
 
 }
+{/**6 操作方法:concat(),slice(),splice() 
+    * concat(),不改变原来数组，返回新数组
+    * slice(),不改变原数组，只有一个参数时，返回从谇参数开始到当前数组末尾的所有项，
+    *           如果有2个参数，返回起始和结束位置之间的项，但不包括结束位置的项
+    * splice(),可以删除，插入，替换;
+    *       改变原来的数组，返回一个数组包含原数组中删除的项，如果没有删除则返回一个空数组*/
+    let colors = ['red','green','blue'];
+    let colors2 = colors.concat('yello',['while','black']);
+    console.log(colors);//[ 'red', 'green', 'blue' ]
+    console.log(colors2);//[ 'red', 'green', 'blue', 'yello', 'while', 'black' ]
+
+    //slice()
+    let col = [ 'red', 'green', 'blue', 'yello', 'while', 'black' ];
+    let col2 = col.slice(2,4);
+    let col3 = col.slice(1);
+    console.log(col);//[ 'red', 'green', 'blue', 'yello', 'while', 'black' ]
+    console.log(col2);//[ 'blue', 'yello' ]
+    console.log(col3);//[ 'green', 'blue', 'yello', 'while', 'black' ]
+
+    //splice()
+    let person = ['age','action','language','color'];
+    let removed = person.splice(0,1); //删除第一项
+    console.log(person);//['action','language','color']
+    console.log(removed);//[ 'age' ]
+
+    removed = person.splice(1,0,'man','woman'); //从位置1开始插入两项
+    console.log(person);//[ 'action', 'man', 'woman', 'language', 'color' ]
+    console.log(removed);//[]
+
+    removed = person.splice(1,1,'red','yellow'); //插入两项，删除一项
+    console.log(person);//[ 'action', 'red', 'yellow', 'woman', 'language', 'color' ]
+    console.log(removed);//[ 'man' ]
+}
+{/**7 位置方法：indexOf(),lastIndexOf() 
+    * 返回要查找的项在数组中的位置，没找到返回-1
+    * indexOf(),可接1到2个参数，从数组开头向后查找
+    * lastIndexOf(),可接1到2个参数，从数组末尾向前查找
+    * IE9+支持*/
+    let numbers = [1,2,3,4,5,3,2,5,6];
+    console.log(
+        numbers.indexOf(3),//2
+        numbers.lastIndexOf(3),//5
+        numbers.indexOf(3,4),//5 //从索引4开始查找3的位置 
+        numbers.lastIndexOf(3,4),//2 //从索引4开始，反向查找3的位置
+    );
+
+}
+{/**8 迭代方法：every(),filter(),forEach(),map(),some()
+    * every():对数组中的每一项运行给定函数，如果该函数对每一项都返回true,则返回true
+    * filter():对数组中的每一项运行给定函数，返回该函数会返回true的项组成的数组
+    * forEach():对数组中的每一项运行给定函数，这个方法没有返回值
+    * map():对数组中的每一项运行给定函数，返回每次函数调用的结果组成的数组。
+    * some():对数组中的每一项运行给定函数，如果该函数对任一项返回true,则返回true */
+    let numbers = [1,2,3,4,5,4,3,2,1];
+    let everResult = numbers.every(function(item,index,array){
+        return item>2;
+    })
+    console.log(everResult); //false
+
+    let someResult = numbers.some(function(item,index,array){
+        return item>2; 
+    })
+    console.log(someResult); //true
+
+    let filterResult = numbers.filter(function(item,index,array){
+        return item>2;
+    })
+    console.log(filterResult); //[3,4,5,4,3]
+
+    let mapResult = numbers.map(function(item,index,array){
+        return item*2;
+    })
+    console.log(mapResult); //[2,4,6,8,10,8,6,4,2]
+
+    numbers.forEach(function(item,index,array){
+        //执行某操作
+    })
+}
+{/**9 归并方法：reduce(),reduceRight() 
+    * 这两个方法都会迭代数组的所有项，然后构建一个最终返回的值
+    * reduce()，从数组的第一项开始，逐个遍历到最后
+    * reduceRight(),从数组的最后一项开始，向前遍历到第一项*/
+    let val = [1,2,3,4,5,6];
+    let sum = val.reduce(function(prev,cur,index,array){
+        //console.log(prev,cur)
+        return prev+cur;
+    });
+    console.log(sum);//21
+
+    let sum2 = val.reduceRight(function(prev,cur,index,array){
+        //console.log(prev,cur);
+        return prev+cur;
+    })
+    console.log(sum2);//21
+}
