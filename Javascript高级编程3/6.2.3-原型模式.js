7809450776ff1568242b7ac947211e9299d54cce
livaha； <1192137406@qq.com>
2019-08-01 11:57:48 +0800

    Update 6.2.3-原型模式.js

--- Javascript高级编程3/6.2.3-原型模式.js
+++ Javascript高级编程3/6.2.3-原型模式.js
@@ -1,6 +1,7 @@
 /**
  * 原型模式(prototype)
  * 1 理解原型：prototype，isPrototypeOf(),getPrototypeOf(),hasOwnProperty()
+ * 2 原型与in操作符 :for-in, Object.keys(),Object.getOwnPropertyNames(),自定义hasPrototypeProperty()
  *  
  * 我们创建的每个函数都有一个prototype（原型）属性,这个属性是一个指针，指向一个对象，而这个对象的用途是包含可以由特定类型的所有实例共享的属性和方法。
  * 按字面理解：prototype是通过调用构造函数而创建的那个对象实例的原型对象。使用原型对象的好处是可以让所有对象实例共享它所包含的属性和方法。
